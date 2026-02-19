e5a2a3631434cf584b7915e02090fcf0f17d4416
Cedrick <cedrickced820@gmail.com>
2026-02-19 14:27:37 +0800

    Implement database connection and roles

--- server/server.js
+++ server/server.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express'); 
 const app = express(); 
 
+const { db } = require('./config/connectDB')
+
 const PORT = process.env.PORT; 
 
-app.use(express.static(path.join(__dirname, 'client', 'pages'))); 
+app.use(express.static(path.join(__dirname, 'client', 'public'))); 
 app.use(express.json()); 
 
-app.listen(PORT, ()=>{
-  console.log(`Server running on PORT: ${PORT}`)
-}); 
+// confirmation of connection to DATABASE and Server
+db.query('SELECT NOW()', (err, res)=>{
+  if(err) return console.log(`Database Connection Failed ${err}`); 
+
+  app.listen(PORT, ()=>{
+    console.log(`Server running to PORT: ${PORT}`)
+  }); 
+  console.log(`Database Connected to PORT: ${process.env.DB_PORT}`)
+});   
