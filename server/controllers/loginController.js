310ef661a633626b944fd5e57f9b7bdcd6394090
Cedrick <cedrickced820@gmail.com>
2026-03-05 22:43:43 +0800

    ADD: Initialize log-in controller

---
+++ server/controllers/loginController.js
@@ -0,0 +1,11 @@
+require('dotenv').config(); 
+const jwt = require('jwt'); 
+const joi = require('joi'); 
+
+function validateInput(email, password){
+  
+}
+
+async function handleLogin(req, res){
+  
+}
